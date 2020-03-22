7067816b3544e59cf4e2b8c173cd3bc6b3433b79
BertFrontEnd <Berton@yandex.ru>
2020-03-22 23:57:40 +0300

    feat: add solving dream-team of BasicJS task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,26 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let str = '';
+  let arr = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] != 'string') {
+      continue;
+    }
+    arr.push(members[i].trim());
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toUpperCase();
+  }
+
+  arr = arr.sort();
+
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i][0].toUpperCase();
+  }
+  return str;
 };
