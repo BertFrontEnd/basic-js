8671f7bd2aa5c4290e9979db24bc7d73833addf1
BertFrontEnd <Berton@yandex.ru>
2021-03-10 18:36:13 +0300

    fix: edit carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,17 +6,13 @@ const HALF_LIFE_PERIOD = 5730;
 module.exports = function dateSample(sampleActivity) {
   if (
     typeof sampleActivity === 'string' &&
-    !isNaN(sampleActivity) &&
-    sampleActivity !== ' ' &&
-    sampleActivity !== '' &&
-    !isNaN(sampleActivity) &&
-    sampleActivity !== ' \n\t\r' &&
-    sampleActivity > 0
+    +sampleActivity &&
+    +sampleActivity > 0 &&
+    +sampleActivity < 15
   ) {
-    let number = parseInt(sampleActivity);
-    /* console.log(number); */
-    let t = Math.log(MODERN_ACTIVITY / number) / (0.693 / HALF_LIFE_PERIOD);
-    return Math.ceil(t);
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let a = MODERN_ACTIVITY / sampleActivity;
+    return Math.ceil(Math.log(a) / k);
   } else {
     return false;
   }
