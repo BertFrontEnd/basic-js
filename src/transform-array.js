748f272827ca5f1913fdce963261a100670ec057
BertFrontEnd <Berton@yandex.ru>
2021-03-23 21:27:38 +0300

    fix: fix transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,27 +5,29 @@ module.exports = function transform(arr) {
     throw new Error('error: "arr" is not Array');
   }
 
-  let newArr = [...arr];
+  let resultArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '--discard-next') {
-      newArr.splice(i, 2);
-      return newArr;
-    }
-
     if (arr[i] === '--discard-prev') {
-      newArr.splice(i - 1, 2);
-      return newArr;
-    }
-
-    if (arr[i] === '--double-next') {
-      newArr.splice(i, 1, arr[i + 1]);
-      return newArr;
-    }
-
-    if (arr[i] === '--double-prev') {
-      newArr.splice(i, 1, arr[i - 1]);
-      return newArr;
+      if (i !== 0 && arr[i - 2] !== '--discard-next') {
+        resultArr.pop();
+      }
+    } else if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1) {
+        resultArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0) {
+        if (arr[i - 2] !== '--discard-next') {
+          resultArr.push(resultArr[resultArr.length - 1]);
+        }
+      }
+    } else {
+      resultArr.push(arr[i]);
     }
   }
+
+  return resultArr;
 };
