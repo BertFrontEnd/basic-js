99b1c88dc6913e061dab9fe310f741108f095a26
BertFrontEnd <Berton@yandex.ru>
2021-03-30 20:55:51 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,84 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    if (type) {
+      this.type = 'direct';
+    } else {
+      this.type = 'reverse';
+    }
+  }
+
+  encrypt(message, cipher) {
+    if (!message || !cipher) {
+      throw Error('No message or cipher');
+    }
+
+    const symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let string = message.toUpperCase();
+    let code = cipher.toUpperCase();
+
+    let k = 0;
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+      if (symbols.indexOf(string[i]) >= 0) {
+        symbols.indexOf(code[k]) + symbols.indexOf(string[i]) >= 26
+          ? (result +=
+              symbols[
+                symbols.indexOf(code[k]) + symbols.indexOf(string[i]) - 26
+              ])
+          : (result +=
+              symbols[symbols.indexOf(code[k]) + symbols.indexOf(string[i])]);
+
+        k >= code.length - 1 ? (k = 0) : k++;
+      } else {
+        result += string[i];
+      }
+    }
+
+    if (this.type === 'reverse') {
+      return result.split('').reverse().join('');
+    } else {
+      return result;
+    }
+  }
+
+  decrypt(message, cipher) {
+    if (!message || !cipher) {
+      throw Error('No message or cipher');
+    }
+
+    const symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let string = message.toUpperCase();
+    let code = cipher.toUpperCase();
+
+    let k = 0;
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+      if (symbols.indexOf(string[i]) >= 0) {
+        symbols.indexOf(string[i]) - symbols.indexOf(code[k]) >= 0
+          ? (result +=
+              symbols[symbols.indexOf(string[i]) - symbols.indexOf(code[k])])
+          : (result +=
+              symbols[
+                symbols.indexOf(string[i]) - symbols.indexOf(code[k]) + 26
+              ]);
+
+        k >= code.length - 1 ? (k = 0) : k++;
+      } else {
+        result += string[i];
+      }
+    }
+
+    if (this.type === 'reverse') {
+      return result.split('').reverse().join('');
+    } else {
+      return result;
+    }
   }
 }
 
